6172119112de5d8a5f8e2e701bc5dd1280ee3568
prolic <saschaprolic@googlemail.com>
2012-06-26 23:04:00 +0200

    [zen-52] Renamed abstract classes in Zend\Service
    Renamed abstract classes
    Fixed exceptions
    Renamed interfaces
    Fixed phpdoc

--- library/ZendService/StrikeIron/Exception/ExceptionInterface.php
+++ library/ZendService/StrikeIron/Exception/ExceptionInterface.php
@@ -19,7 +19,7 @@
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 
-namespace Zend\Service\StrikeIron;
+namespace Zend\Service\StrikeIron\Exception;
 
 /**
  * @category   Zend
@@ -28,5 +28,5 @@ namespace Zend\Service\StrikeIron;
  * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-interface Exception
+interface ExceptionInterface
 {}
